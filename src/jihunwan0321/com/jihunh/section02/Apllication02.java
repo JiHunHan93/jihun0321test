bd24b169d06f5c2fb9dc1f2d13a5807989749eb9
JiHunHan93 <74941254+JiHunHan93@users.noreply.github.com>
2021-03-21 20:07:00 +0900

    이렇게 해결하나?

--- src/jihunwan0321/com/jihunh/section02/Apllication02.java
+++ src/jihunwan0321/com/jihunh/section02/Apllication02.java
@@ -5,8 +5,7 @@ public class Apllication02 {
 	public static void main(String[] args) {
 		
 		System.out.println("이건 어디다 써야할까?");
-		System.out.println("스위치를 Master로 push행위");
-		
+		System.out.println("이건 초록색인가?");
 	}
 
 }
