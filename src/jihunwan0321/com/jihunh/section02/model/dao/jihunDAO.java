c92865b84fdd1ecd905ac1cdb5e201ded9058e5f
DESKTOP-TQ5I0BH\WANGYU <kordzor@gmail.com>
2021-03-21 19:26:56 +0900

    넘무어려운것

---
+++ src/jihunwan0321/com/jihunh/section02/model/dao/jihunDAO.java
@@ -0,0 +1,10 @@
+package jihunwan0321.com.jihunh.section02.model.dao;
+
+import java.util.List;
+
+import jihunwan0321.com.greedy.jihun.run.jihunDTO;
+
+public interface jihunDAO {
+
+	List<jihunDTO> jilist();
+}
---
+++ src/jihunwan0321/com/jihunh/section02/model/dao/jihunDAOImpl.java
@@ -0,0 +1,15 @@
+package jihunwan0321.com.jihunh.section02.model.dao;
+
+import java.util.List;
+
+import jihunwan0321.com.greedy.jihun.run.jihunDTO;
+
+public class jihunDAOImpl implements jihunDAO {
+
+	@Override
+	public List<jihunDTO> jilist() {
+		System.out.println("또까먹음");
+		return null;
+	}
+
+}
