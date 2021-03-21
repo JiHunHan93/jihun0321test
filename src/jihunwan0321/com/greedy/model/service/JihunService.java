c554c9166a7a55381b51b8c6c9ebc247df03e09d
JiHunHan93 <74941254+JiHunHan93@users.noreply.github.com>
2021-03-21 19:29:22 +0900

    서비스등록

---
+++ src/jihunwan0321/com/greedy/model/service/JihunService.java
@@ -0,0 +1,11 @@
+package jihunwan0321.com.greedy.model.service;
+
+import java.util.List;
+
+import jihunwan0321.com.jihunh.section02.model.dto.JihunDTO;
+
+public interface JihunService {
+	
+	List<JihunDTO> select();
+	
+}
